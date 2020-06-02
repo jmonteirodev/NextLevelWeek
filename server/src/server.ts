98bf39951756b367c0c15e72be606efd2dfc750d
Jonathan Monteiro <jonathanmonteirodev@gmail.com>
2020-06-02 11:05:33 -0300

    Primeiro dia

---
+++ server/src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/user', (request, response) => {
+    console.log('Listagem de usuários');
+    response.json(['Diego','Cleiton','Robson', 'Daniel']);
+});
+app.listen(3333);
